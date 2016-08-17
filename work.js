ae6258a35b9431ae4250b26ddefed6988fa7e6c9
Tim Davis <contact@timdav.is>
2016-08-16 23:07:29 -0700

    closes #2

--- work.js
+++ work.js
@@ -1,15 +1,18 @@
 var { store, excavate, DirtError, logError } = require('./helpers')
 var { user } = require('./user.json')
 
-function work() {
-  excavate()
-    .then(payload => {
-      if (payload.gold) return payload
-      return Promise.reject(new DirtError())
-    })
-    .then(({ bucketId }) => store(user, bucketId))
-    .catch(logError)
-    .then(work)
+var work = workers => () => {
+
+  Array(workers).fill().forEach(() => {
+    excavate()
+      .then(payload => {
+        if (payload.gold) return payload
+        return Promise.reject(new DirtError())
+      })
+      .then(({ bucketId }) => store(user, bucketId))
+      .catch(logError)
+      .then(work(1))
+  })
 }
 
-work()
+work(10)()
